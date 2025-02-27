c84399a6d842c7055a2ed92a4f0ede8222dc76ab
Babur Makhmudov <bmuddha13@gmail.com>
2025-02-27 22:35:15 +0400

    feat: add ShmemQueue constructor

---
+++ src/error.rs
@@ -0,0 +1,24 @@
+pub enum ShmemError {
+    Open(i32),
+    Resize(i32),
+    Mmap(i32),
+}
+
+#[macro_export]
+macro_rules! inspecterr {
+    ($val: expr, $variant: ident) => {
+        inspecterr!($val, $variant, -1)
+    };
+    ($val: expr, $variant: ident, $err: expr) => {{
+        #[cfg(target_os = "linux")]
+        use libc::__errno_location as errno_location;
+
+        #[cfg(target_os = "macos")]
+        use libc::__error as errno_location;
+
+        if $val == $err {
+            let errcode = unsafe { *errno_location() };
+            return Err(ShmemError::$variant(errcode));
+        }
+    }};
+}
